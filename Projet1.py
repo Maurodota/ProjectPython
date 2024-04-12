dd8ecb088a7f69d3e8ddc95468de0e3d66117762
Dota <80065406+Maurodota@users.noreply.github.com>
2024-04-12 13:38:26 -0300

    Delete Projet1.py

--- Projet1.py
+++
@@ -1,7 +0,0 @@
-# Sistema de Liberação para maior idade.
-
-MAIOR_IDADE = 18
-idade = int(input("Informe sua idade: "))
-
-status = "OK" if idade >= MAIOR_IDADE else "Não"
-print (f"{status} atende aos requisitos")
